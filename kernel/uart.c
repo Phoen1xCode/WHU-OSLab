4651cf43685452b0c3eec81a86a591a152f22a8d
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-09-15 21:29:20 +0800

    feat: add uart.c for minimal uart driver of 16550a UART.

---
+++ kernel/uart.c
@@ -0,0 +1,40 @@
+//
+// minimal uart driver for 16550a UART.
+//
+
+
+#include "types.h"
+#include "memlayout.h" 
+
+
+// the UART control registers are memory-mapped
+// at address UART0. this macro returns the
+// address of one of the registers.
+// #define UART0 0x10000000L
+#define Reg(reg) ((volatile unsigned char *)(UART0 + (reg)))
+#define ReadReg(reg) (*(Reg(reg)))
+#define WriteReg(reg, v) (*(Reg(reg)) = (v))
+
+#define RHR 0 // receive holding register (for input bytes)
+#define THR 0 // transmit holding register (for output bytes)
+
+#define LSR 5 // line status register
+#define LSR_RX_READY (1<<0) // input is waitting to be read from RHR
+#define LSR_TX_IDLE (1<<5) // THR can accept another character to send
+
+
+void uart_putc(char c) {
+    // wait until THR is ready to send
+    while ((ReadReg(LSR) & LSR_TX_IDLE) == 0)
+        ; // busy wait until THR is ready to send
+
+    // write the character to the THR register
+    WriteReg(THR, c);
+}
+
+void uart_puts(char *s) {
+    while (*s != '\0') {
+        uart_putc(*s);
+        s++;
+    }
+}
