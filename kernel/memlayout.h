f20d865613cff914fdade16dab356e62e0fea9a3
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-09-28 14:16:12 +0800

    feat: define KERNBASE and PHYSTOP for kernel memory layout

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -12,3 +12,9 @@
 
 // qemu puts UART registers here in physical memory.
 #define UART0 0x10000000L
+
+// the kernel expects there to be RAM
+// for use by the kernel and user pages
+// from physical address 0x80000000 to PHYSTOP.
+#define KERNBASE 0x80000000L
+#define PHYSTOP (KERNBASE + 128*1024*1024)
