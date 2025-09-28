84118900b7a4d482f46559f613d4166294453386
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-09-28 14:40:21 +0800

    fix: remove clear_screen() call from start function

--- kernel/start.c
+++ kernel/start.c
@@ -3,7 +3,6 @@
 
 void start(void) {
   printf("Booting... %d %x %s %c %%\n", 42, 0x2a, "ok", 'A');
-  clear_screen();
   printf("Screen cleared. Hello OS!\n");
   for (;;) {
     __asm__ volatile("wfi");
