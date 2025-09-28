cfaefa5ca3e8c6a4941eb0d709ad05e0cded464a
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-09-28 14:40:56 +0800

    feat: include types.h and organize function declarations in defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -1,4 +1,4 @@
-
+#include "types.h"
 // console.c
 void consputc(int c);
 void consputs(const char *s);
@@ -6,8 +6,19 @@ void clear_screen(void);
 
 // printf.c
 int printf(const char *fmt, ...);
+void panic(char *) __attribute__((noreturn));
 
 
 // uart.c
 void uartputc(char c);
 void uartputs(const char *s);
+
+
+// string.c
+int             memcmp(const void*, const void*, uint);
+void*           memmove(void*, const void*, uint);
+void*           memset(void*, int, uint);
+char*           safestrcpy(char*, const char*, int);
+int             strlen(const char*);
+int             strncmp(const char*, const char*, uint);
+char*           strncpy(char*, const char*, int);
