f45fda8b24d1bbaae03575cb1003cae2279b6d75
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-11-10 23:11:06 +0800

    refactor(spinlock): improve formatting and readability in release function

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -34,21 +34,21 @@ void acquire(struct spinlock *lock) {
 }
 
 void release(struct spinlock *lock) {
-    if(!holding(lock))
-        panic("release");
-    
-    lock->cpu = 0;
+  if (!holding(lock))
+    panic("release");
 
-    // Tell the C compiler and the CPU to not move loads or stores
-    // past this point, to ensure that all the stores in the critical
-    // section are visible to other CPUs before the lock is released,
-    // and that loads in the critical section occur strictly before
-    // the lock is released.
-    // On RISC-V, this emits a fence instruction.
-    __sync_synchronize();
+  lock->cpu = 0;
 
-    lock->locked = 0; // 释放锁
-    pop_off(); // 恢复中断
+  // Tell the C compiler and the CPU to not move loads or stores
+  // past this point, to ensure that all the stores in the critical
+  // section are visible to other CPUs before the lock is released,
+  // and that loads in the critical section occur strictly before
+  // the lock is released.
+  // On RISC-V, this emits a fence instruction.
+  __sync_synchronize();
+
+  lock->locked = 0; // 释放锁
+  pop_off();        // 恢复中断
 }
 
 // Check whether this cpu is holding the lock.
