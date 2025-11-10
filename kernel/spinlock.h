6ba1b062711409090aa1505dfd2d57952cb0b023
Phoen1xCode <cnphoen1xcode@gmail.com>
2025-11-11 00:18:33 +0800

    refactor(spinlock): improve formatting and consistency in spinlock structure

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,8 +1,13 @@
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
+
 // Mutual exclusion spin lock.
 struct spinlock {
-    int locked;       // Is the lock held?
+  int locked; // Is the lock held?
 
-    // For debugging:
-    char *name;        // Name of lock.
-    struct cpu *cpu;   // The cpu holding the lock.
+  // For debugging:
+  char *name;      // Name of lock.
+  struct cpu *cpu; // The cpu holding the lock.
 };
+
+#endif // SPINLOCK_H
